70e9c4ede84be856643276c48c7c5e77810841a1
Dant3s15 <82174072+Dant3s15@users.noreply.github.com>
2021-06-19 18:18:37 +0200

    commit

---
+++ app/js/javascript.js
@@ -0,0 +1,16 @@
+const hamburger = document.getElementById('hamburger');
+
+hamburger.addEventListener('click', () =>{
+    
+})
+
+
+// function hamburgerMenu(){
+//     var x = document.getElementById("Hamburger");
+//     if (x.style.display === "block"){
+//         x.style.display = "none";
+//     }else{
+//         x.style.display = "block";
+//     }
+    
+// }
