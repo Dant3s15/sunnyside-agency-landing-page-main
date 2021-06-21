7e91f69421ed045ec109c6084c6a59b36df84591
Dant3s15 <82174072+Dant3s15@users.noreply.github.com>
2021-06-21 20:35:15 +0200

    commit

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -1,10 +1,10 @@
 const hamburger = document.querySelector('#hamburger');
 const nav = document.querySelector('#nav');
-const mediaQuery = window.matchMedia("(min-width: 769px)");
+// const mediaQuery = window.matchMedia("(min-width: 769px)");
 
 hamburger.addEventListener('click', function(){
 
-    if ((nav.classList.contains('nav-mobile')) || (mediaQuery.matches)){
+    if (nav.classList.contains('nav-mobile')){
 
         nav.classList.remove('nav-mobile')
         nav.classList.add('nav')
